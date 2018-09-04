0096250aacb20be19b8ff04bd2a680294e930b68
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2018-09-04 09:37:52 +0200

    Setting the Default Route

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -36,8 +36,12 @@ namespace SportsStore
             app.UseStaticFiles();
 
             // enables ASP.NET Core MVC
+            // The UseMvc method sets up the MVC middleware, and one of the configuration options is the scheme that will be used to map URLs to controllers and action method
             app.UseMvc(routes => {
-                
+                routes.MapRoute(
+                    name: "default",
+                    // tells MVC to send requests to the List action method of the Product controller unless the request URL specifies otherwise
+                    template: "{controller=Product}/{action=List}/{id?}");                
             });
         }
     }
