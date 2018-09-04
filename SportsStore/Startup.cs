75949e0b5b4205ce76aaf053914e1798a686ce21
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2018-09-04 08:32:01 +0200

    Configuring the Application - Startup.cs

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -13,21 +13,28 @@ namespace SportsStore
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // The ConfigureServices method is used to set up shared objects that can be used throughout the application through the dependency injection feature
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(); // sets up the shared objects used in MVC applications
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // The Configure method is used to set up the features that receive and process HTTP requests.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // displays details of exceptions that occur in the application, which is useful during the development process. It should not be enabled in deployed applications
+            app.UseDeveloperExceptionPage();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            // adds a simple message to HTTP responses that would not otherwise have a body, such as 404 - Not Found responses
+            app.UseStatusCodePages();
+
+            // enables support for serving static content from the wwwroot folder, ej. pdf
+            app.UseStaticFiles();
+
+            // enables ASP.NET Core MVC
+            app.UseMvc(routes => {
+                
             });
         }
     }
