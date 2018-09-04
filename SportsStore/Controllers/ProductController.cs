e856cde23cd2be12b06fd0303feecddd44414341
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2018-09-04 21:22:57 +0200

    Adding the View Model Data

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 using System.Linq;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
@@ -19,10 +20,24 @@ namespace SportsStore.Controllers
         //Will render a view showing the complete list of the products in the repository
         //Calling the View method like this (without specifying a view name) tells MVC to render the default view for the action method. Passing the collection of Product objects from the repository to the View method provides the framework with the data with which to populate the Model object in a strongly typed view.
         //I have added an optional parameter to the List method, which means that if I call the method without a parameter (List()), my call is treated as though I had supplied the value specified in the parameter definition (List(1)). The effect is that the action method displays the first page of products when MVC invokes it without an argument. Within the body of the action method, I get the Product objects, order them by the primary key, skip over the products that occur before the start of the current page, and take the number of products specified by the PageSize field. 
-        public ViewResult List(int productPage = 1) 
-            => View(repository.Products
-                .OrderBy(p => p.ProductID)
-                .Skip((productPage - 1) * PageSize)
-                .Take(PageSize));
+        //update the List action method in the ProductController class to use the 
+        //ProductsListViewModel class to provide the view with details of the products to display on the page and   
+        //details of the pagination
+        public ViewResult List(int productPage = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
+        //These changes pass a ProductsListViewModel object as the model data to the view.
+
     }
 }
---
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SportsStore.Models;
+
+namespace SportsStore.Models.ViewModels
+{
+    //send from the controller to the view in a single view model class 
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
