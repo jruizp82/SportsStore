1a2dc10b4c03430679baff1b99278822707e60d0
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2018-09-04 10:50:07 +0200

    Read the connection string and to configure the application to use it to connect to the database

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -7,18 +7,31 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsStore
 {
     public class Startup
     {
+        // receives the configuration data loaded from the appsettings.json file, which is presented through an object that implements the IConfiguration interface. The constructor assigns the IConfiguration object to a property called Configuration so that it can be used by the rest of the Startup class
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         // The ConfigureServices method is used to set up shared objects that can be used throughout the application through the dependency injection feature
         public void ConfigureServices(IServiceCollection services)
         {
-            // Tells ASP.NET Core that when a component, such as a controller, needs an implementation of the IProductRepository interface, it should receive an instance of the FakeProductRepository class. The AddTransient method specifies that a new FakeProductRepository object should be created each time the IProductRepository interface is needed.
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            // sets up the services provided by Entity Framework Core for the database context class.
+            //  configured the database with the UseSqlServer method and specified the connection string, which is obtained from the Configuration property.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration["Data:SportsStoreProducts:ConnectionString"]));
+            // The components in the application that use the IProductRepository interface, which is just the Product controller at the moment, will receive an EFProductRepository object when they are created, which will provide them with access to the data in the database
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc(); // sets up the shared objects used in MVC applications
         }
 
