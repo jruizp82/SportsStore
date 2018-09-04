c0eaa16646ba3d2dbb10325cb5e11b63f5e8a758
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2018-09-04 10:09:30 +0200

    Creating the Repository Class

---
+++ SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    // for the moment, the repository implementation just maps the Products property defined by the IProductRepository interface onto the Products property defined by the ApplicationDbContext class. The Products property in the context class returns a DbSet<Product> object, which implements the IQueryable<T> interface and makes it easy to implement the IProductRepository interface when using Entity Framework Core. This ensures that queries to the database will retrieve only the objects that are required
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Product> Products => context.Products;
+    }
+}
