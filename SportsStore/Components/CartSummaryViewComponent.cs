36094581622843ea4d3e9a3b5758c6b8ebbee5b7
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2018-09-18 20:53:50 +0200

    Creating the View Component Class and View

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+namespace SportsStore.Components
+{
+    //This view component is able to take advantage of the service that I created earlier in the chapter in order
+    //to receive a Cart object as a constructor argument.The result is a simple view component class that passes
+    //on the Cart object to the View method in order to generate the fragment of HTML that will be included in the layout.
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
