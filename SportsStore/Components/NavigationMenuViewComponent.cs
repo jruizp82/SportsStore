79802b3316c2ef6155a8497ed8ff11897ef82c62
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2018-09-18 18:44:52 +0200

    Creating the Navigation View Component

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        //The view component’s Invoke method is called when the component is used in a Razor view, and the
+        //result of the Invoke method is inserted into the HTML sent to the browser
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
