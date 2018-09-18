e307fc2ad5065c33d35092081d27e981d72c81d4
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2018-09-18 20:15:53 +0200

    Refining the Cart Model with a Service Creating a Storage-Aware Cart Class

---
+++ SportsStore/Models/SessionCart.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using SportsStore.Infrastructure;
+namespace SportsStore.Models
+{
+
+    //The SessionCart class subclasses the Cart class and overrides the AddItem, RemoveLine, and Clear
+    //methods so they call the base implementations and then store the updated state in the session using the
+    //extension methods on the ISession interface
+
+    public class SessionCart : Cart
+    {
+        //The static GetCart method is a factory
+        //for creating SessionCart objects and providing them with an ISession object so they can store themselves.
+        public static Cart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
+                .HttpContext.Session;
+            SessionCart cart = session?.GetJson<SessionCart>("Cart")
+                ?? new SessionCart();
+            cart.Session = session;
+            return cart;
+        }
+
+        [JsonIgnore]
+        public ISession Session { get; set; }
+        public override void AddItem(Product product, int quantity)
+        {
+            base.AddItem(product, quantity);
+            Session.SetJson("Cart", this);
+        }
+        public override void RemoveLine(Product product)
+        {
+            base.RemoveLine(product);
+            Session.SetJson("Cart", this);
+        }
+        public override void Clear()
+        {
+            base.Clear();
+            Session.Remove("Cart");
+        }
+    }
+}
