d009cb98fb8644f63d483d5579544d607510872f
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2018-09-18 19:04:20 +0200

    UNIT TEST: REPORTING THE SELECTED CATEGORY

--- SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Routing;
 using Moq;
 using SportsStore.Components;
 using SportsStore.Models;
@@ -13,7 +15,7 @@ namespace SportsStore.Tests
         //I created a mock repository implementation that contains repeating categories and categories that are
         //not in order.i assert that the duplicates are removed and that alphabetical ordering is imposed.
 
-       [Fact]
+        [Fact]
         public void Can_Select_Categories()
         {
             // Arrange
@@ -27,14 +29,44 @@ namespace SportsStore.Tests
 
             NavigationMenuViewComponent target =
                 new NavigationMenuViewComponent(mock.Object);
-            
+
             // Act = get the set of categories
             string[] results = ((IEnumerable<string>)(target.Invoke()
                 as ViewViewComponentResult).ViewData.Model).ToArray();
-            
+
             // Assert
             Assert.True(Enumerable.SequenceEqual(new string[] { "Apples",
                 "Oranges", "Plums" }, results));
+        }
+
+        [Fact]
+        public void Indicates_Selected_Category()
+        {
+            // Arrange
+            string categoryToSelect = "Apples";
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductID = 4, Name = "P2", Category = "Oranges"},
+            }).AsQueryable<Product>());
+            NavigationMenuViewComponent target =
+                new NavigationMenuViewComponent(mock.Object);
+
+            target.ViewComponentContext = new ViewComponentContext
+            {
+                ViewContext = new ViewContext
+                {
+                    RouteData = new RouteData()
+                }
+            };
+            target.RouteData.Values["category"] = categoryToSelect;
+
+            // Action
+            string result = (string)(target.Invoke() as
+                ViewViewComponentResult).ViewData["SelectedCategory"];
+
+            // Assert
+            Assert.Equal(categoryToSelect, result);
         }
     }
 }
