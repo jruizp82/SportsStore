f274558d4d3d8408f74ab0b90e4b3471cc64c306
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2018-09-18 19:41:38 +0200

    Adding the Add to Cart Buttons

---
+++ SportsStore/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace SportsStore.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        //operates on the HttpRequest class, which ASP.NET uses to
+        //describe an HTTP request.The extension method generates a URL that the browser will be returned to after
+        //the cart has been updated, taking into account the query string if there is one
+        public static string PathAndQuery(this HttpRequest request) =>
+            request.QueryString.HasValue
+                ? $"{request.Path}{request.QueryString}"
+                : request.Path.ToString();
+    }
+}
