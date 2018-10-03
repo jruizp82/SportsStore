551253b94e7caa3bac00fd6c5a85c229ca8f595e
Juan Antonio Ruiz Pintor <ruiz.ja.1982@gmail.com>
2018-10-03 12:47:04 +0200

    Add Checkout process 2

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        //The Checkout method returns the default view and passes a new ShippingDetails object as the view
+        //model.
+        public ViewResult Checkout() => View(new Order());
+    }
+}
